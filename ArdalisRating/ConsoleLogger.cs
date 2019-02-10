05fd42bdc36aaab063489d3f37dbfe32366ddca7
Steve Smith <steve@kentsmiths.com>
2019-02-10 15:46:45 -0500

    Applying SRP

---
+++ ArdalisRating/ConsoleLogger.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.IO;
+
+namespace ArdalisRating
+{
+    public class ConsoleLogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
