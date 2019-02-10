de5a1cc31fa85bbef8a117ee427a602059e8b26c
Steve Smith <steve@kentsmiths.com>
2019-02-10 15:46:45 -0500

    Applying SRP

---
+++ ArdalisRating/FilePolicySource.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.IO;
+
+namespace ArdalisRating
+{
+    public class FilePolicySource
+    {
+        public string GetPolicyFromSource()
+        {
+            return File.ReadAllText("policy.json");
+        }
+    }
+}
--- ArdalisRating/RatingEngine.cs
+++ ArdalisRating/RatingEngine.cs
@@ -1,37 +1,45 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using System;
-using System.IO;
 
 namespace ArdalisRating
 {
+    public class JsonPolicySerializer
+    {
+        public Policy GetPolicyFromJsonString(string jsonString)
+        {
+            return JsonConvert.DeserializeObject<Policy>(jsonString,
+                new StringEnumConverter());
+        }
+    }
     /// <summary>
     /// The RatingEngine reads the policy application details from a file and produces a numeric 
     /// rating value based on the details.
     /// </summary>
     public class RatingEngine
     {
+        public ConsoleLogger Logger { get; set; } = new ConsoleLogger();
+        public FilePolicySource PolicySource { get; set; } = new FilePolicySource();
+        public JsonPolicySerializer PolicySerializer { get; set; } = new JsonPolicySerializer();
         public decimal Rating { get; set; }
         public void Rate()
         {
-            Console.WriteLine("Starting rate.");
+            Logger.Log("Starting rate.");
 
-            Console.WriteLine("Loading policy.");
+            Logger.Log("Loading policy.");
 
-            // load policy - open file policy.json
-            string policyJson = File.ReadAllText("policy.json");
+            string policyJson = PolicySource.GetPolicyFromSource();
 
-            var policy = JsonConvert.DeserializeObject<Policy>(policyJson,
-                new StringEnumConverter());
+            var policy = PolicySerializer.GetPolicyFromJsonString(policyJson);
 
             switch (policy.Type)
             {
                 case PolicyType.Auto:
-                    Console.WriteLine("Rating AUTO policy...");
-                    Console.WriteLine("Validating policy.");
+                    Logger.Log("Rating AUTO policy...");
+                    Logger.Log("Validating policy.");
                     if (String.IsNullOrEmpty(policy.Make))
                     {
-                        Console.WriteLine("Auto policy must specify Make");
+                        Logger.Log("Auto policy must specify Make");
                         return;
                     }
                     if (policy.Make == "BMW")
@@ -45,37 +53,37 @@ namespace ArdalisRating
                     break;
 
                 case PolicyType.Land:
-                    Console.WriteLine("Rating LAND policy...");
-                    Console.WriteLine("Validating policy.");
+                    Logger.Log("Rating LAND policy...");
+                    Logger.Log("Validating policy.");
                     if (policy.BondAmount == 0 || policy.Valuation == 0)
                     {
-                        Console.WriteLine("Land policy must specify Bond Amount and Valuation.");
+                        Logger.Log("Land policy must specify Bond Amount and Valuation.");
                         return;
                     }
                     if (policy.BondAmount < 0.8m * policy.Valuation)
                     {
-                        Console.WriteLine("Insufficient bond amount.");
+                        Logger.Log("Insufficient bond amount.");
                         return;
                     }
                     Rating = policy.BondAmount * 0.05m;
                     break;
 
                 case PolicyType.Life:
-                    Console.WriteLine("Rating LIFE policy...");
-                    Console.WriteLine("Validating policy.");
+                    Logger.Log("Rating LIFE policy...");
+                    Logger.Log("Validating policy.");
                     if (policy.DateOfBirth == DateTime.MinValue)
                     {
-                        Console.WriteLine("Life policy must include Date of Birth.");
+                        Logger.Log("Life policy must include Date of Birth.");
                         return;
                     }
                     if (policy.DateOfBirth < DateTime.Today.AddYears(-100))
                     {
-                        Console.WriteLine("Centenarians are not eligible for coverage.");
+                        Logger.Log("Centenarians are not eligible for coverage.");
                         return;
                     }
                     if (policy.Amount == 0)
                     {
-                        Console.WriteLine("Life policy must include an Amount.");
+                        Logger.Log("Life policy must include an Amount.");
                         return;
                     }
                     int age = DateTime.Today.Year - policy.DateOfBirth.Year;
@@ -95,11 +103,11 @@ namespace ArdalisRating
                     break;
 
                 default:
-                    Console.WriteLine("Unknown policy type");
+                    Logger.Log("Unknown policy type");
                     break;
             }
 
-            Console.WriteLine("Rating completed.");
+            Logger.Log("Rating completed.");
         }
     }
 }
