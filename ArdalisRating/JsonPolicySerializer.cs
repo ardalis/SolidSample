524cde04f414123752a50a65418311219f3894f5
Steve Smith <steve@kentsmiths.com>
2019-02-11 09:28:16 -0500

    Moved class to separate file

---
+++ ArdalisRating/JsonPolicySerializer.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace ArdalisRating
+{
+    public class JsonPolicySerializer
+    {
+        public Policy GetPolicyFromJsonString(string jsonString)
+        {
+            return JsonConvert.DeserializeObject<Policy>(jsonString,
+                new StringEnumConverter());
+        }
+    }
+}
