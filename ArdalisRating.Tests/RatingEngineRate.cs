3fa793dd8cc94b6cd05f1e61e8b2f69a9dd852a9
Steve Smith <steve@kentsmiths.com>
2019-02-11 09:47:04 -0500

    Adding test project and 2 tests

---
+++ ArdalisRating.Tests/RatingEngineRate.cs
@@ -0,0 +1,52 @@
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using Xunit;
+
+namespace ArdalisRating.Tests
+{
+    public class RatingEngineRate
+    {
+        [Fact]
+        public void ReturnsRatingOf10000For200000LandPolicy()
+        {
+            var policy = new Policy
+            {
+                Type = PolicyType.Land,
+                BondAmount = 200000,
+                Valuation = 200000
+            };
+            string json = JsonConvert.SerializeObject(policy);
+            File.WriteAllText("policy.json", json);
+
+            var engine = new RatingEngine();
+
+            engine.Rate();
+
+            var result = engine.Rating;
+
+            Assert.Equal(10000, result);
+        }
+
+        [Fact]
+        public void ReturnsRatingOf0For200000BondOn260000LandPolicy()
+        {
+            var policy = new Policy
+            {
+                Type = PolicyType.Land,
+                BondAmount = 200000,
+                Valuation = 260000
+            };
+            string json = JsonConvert.SerializeObject(policy);
+            File.WriteAllText("policy.json", json);
+
+            var engine = new RatingEngine();
+
+            engine.Rate();
+
+            var result = engine.Rating;
+
+            Assert.Equal(0, result);
+        }
+    }
+}
